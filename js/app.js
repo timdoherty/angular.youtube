b5aaeade2ae57fec73999d67a3d9f789a7f9732b
tdoherty <tim@dohertycomputing.com>
2013-03-13 07:14:26 -0700

    update from master

--- js/app.js
+++ js/app.js
@@ -7,12 +7,11 @@ define(function (require) {
   var filters = require('./filters/filters');
   var services = require('./services/services');
   var directives = require('./directives/directives');
-  var MyCtrl1 = require('./controllers/c1');
-  var MyCtrl2 = require('./controllers/c2');
   var youTubeCtrl = require('./controllers/youTube');
   require('./directives/appVersion');
   require('./directives/searchResult');
   require('./directives/relatedVideo');
+  require('./directives/pager');
   require('./directives/comment');
   require('./directives/keyPress');
   require('./filters/interpolate');
@@ -23,8 +22,8 @@ define(function (require) {
   m.config(['$routeProvider', function($routeProvider) {
 //    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'myCtrl1'});
 //    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'myCtrl2'});
-    $routeProvider.when('/', {templateUrl: 'partials/partial3.html', controller: 'YouTubeCtrl'});
-    $routeProvider.when('/:searchTerm', {templateUrl: 'partials/partial3.html', controller: 'YouTubeCtrl'});
+    $routeProvider.when('/', {templateUrl: 'partials/search.html', controller: 'YouTubeCtrl'});
+    $routeProvider.when('/:searchTerm', {templateUrl: 'partials/search.html', controller: 'YouTubeCtrl'});
     $routeProvider.when('/video/:videoID', {templateUrl: 'partials/player.html', controller: 'YouTubeCtrl'});
 
     $routeProvider.otherwise({redirectTo: '/'});
